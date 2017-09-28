08b74a6d4c0fa3b00e14bdf0ec811c25e89714eb
Francesco Mazzoli <f@mazzo.li>
2017-09-28 23:01:40 +0200

    time more operations

--- src/Anapo/Loop.hs
+++ src/Anapo/Loop.hs
@@ -3,7 +3,8 @@ module Anapo.Loop where
 import Control.Exception (BlockedIndefinitelyOnMVar, try)
 import Control.Concurrent.Chan (Chan, writeChan, readChan, newChan)
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad (void)
+import Control.Monad (void, when)
+import Data.Time.Clock (getCurrentTime, diffUTCTime, NominalDiffTime)
 
 import qualified GHCJS.DOM as DOM
 import qualified GHCJS.DOM.Document as DOM
@@ -29,8 +30,16 @@ withDispatch = do
         Right f -> return (Just f)
   return (liftIO . writeChan toDispatch, get)
 
+timeIt :: ClientM a -> ClientM (a, NominalDiffTime)
+timeIt m = do
+  t0 <- liftIO getCurrentTime
+  x <- m
+  t1 <- liftIO getCurrentTime
+  return (x, diffUTCTime t1 t0)
+
 componentLoop :: forall state acc.
-     Dispatch state
+     RenderOptions
+  -> Dispatch state
   -> ClientM (Maybe (state -> ClientM state))
   -> state
   -> Component' state
@@ -38,11 +47,13 @@ componentLoop :: forall state acc.
   -> ClientM acc
   -- ^ returns the final accumulator, when there is nothing left to do.
   -- might never terminate
-componentLoop dispatch getStateUpdate !st0 vdom !acc0 useDom = do
+componentLoop ro dispatch getStateUpdate !st0 vdom !acc0 useDom = do
   let
     go :: Maybe state -> state -> acc -> ClientM acc
     go mbPrevSt !st !acc = do
-      nodes <- runComponent vdom dispatch mbPrevSt st
+      (nodes, vdomDt) <- timeIt (runComponent vdom dispatch mbPrevSt st)
+      when (roDebugOutput ro) $
+        liftIO (putStrLn ("Vdom generated (" ++ show vdomDt ++ ")"))
       acc' <- useDom acc nodes
       mbF <- getStateUpdate
       case mbF of
@@ -50,8 +61,8 @@ componentLoop dispatch getStateUpdate !st0 vdom !acc0 useDom = do
           liftIO (putStrLn "No state update received, terminating component loop")
           return acc'
         Just f -> do
-          st' <- f st
-          liftIO (putStrLn "State updated, wil re render")
+          (st', updateDt) <- timeIt (f st)
+          liftIO (putStrLn ("State updated (" ++ show updateDt ++ "),  will re render"))
           go (Just st) st' acc'
   go Nothing st0 acc0
 
@@ -65,7 +76,7 @@ installComponentBody ::
 installComponentBody ro dispatch getStateUpdate st0 vdom0 = do
   doc <- DOM.currentDocumentUnchecked
   body <- DOM.getBodyUnchecked doc
-  void $ componentLoop dispatch getStateUpdate st0 vdom0 Nothing $ \mbVdom vdom -> do
+  void $ componentLoop ro dispatch getStateUpdate st0 vdom0 Nothing $ \mbVdom vdom -> do
     evts <- renderVirtualDom ro doc body mbVdom vdom
     DOM.syncPoint -- force jssaddle stuff
     return (Just (vdom, evts))
@@ -83,7 +94,7 @@ installComponentBootstrap ro dispatch getStateUpdate st0 vdom0 = do
   container <- DOM.unsafeCastTo DOM.HTMLDivElement =<< DOM.createElement doc "div"
   DOM.setClassName container "container"
   DOM.appendChild_ body container
-  void $ componentLoop dispatch getStateUpdate st0 vdom0 Nothing $ \mbVdom vdom -> do
+  void $ componentLoop ro dispatch getStateUpdate st0 vdom0 Nothing $ \mbVdom vdom -> do
     evts <- renderVirtualDom ro doc container mbVdom vdom
     DOM.syncPoint -- force jssaddle stuff
     return (Just (vdom, evts))
